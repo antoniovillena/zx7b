a3c7c355de1575bb3a861d37fa1fe697b6c7b39a
antoniovillena <espineter@yahoo.com>
2018-10-01 17:07:00 +0200

    Añado apc

--- dzx7b_fast.asm
+++ dzx7b_fast.asm
@@ -10,59 +10,64 @@
 
     macro getbitm
       if speed=0
-        add     a, a
-        call    z, getbit
+        add     a, a            ; check next bit
+        call    z, getbit       ; no more bits left?
       else
-        add     a,  a
-        jp      nz, .gb1
-        ld      a, (hl)
+        add     a,  a           ; check next bit
+        jp      nz, .gb1        ; no more bits left?
+        ld      a, (hl)         ; load another group of 8 bits
         dec     hl
         adc     a, a
 .gb1
       endif
     endm
 
-dzx7:   ld      a, $80
+dzx7:   ld      a, $80          ; set marker bit as MSB
       if speed>1
-        ldd
-        add     a, a
-        jr      z, mailab
-        jr      c, maicoo
-        ldd
+        ldd                     ; copy literal byte
+        add     a, a            ; read next bit, faster method
+        jr      z, mailab       ; call routine only if need to load next byte
+        jr      c, maicoo       ; next bit indicates either literal or sequence
+        ldd                     ; loop unrolling x4
         add     a, a
         jr      c, maicoe
       endif
-copbye: ldd
+copbye: ldd                     ; copy literal byte
+        add     a, a            ; read next bit, faster method
+        jr      z, mailab       ; call routine only if need to load next byte
+        jr      c, maicoo       ; next bit indicates either literal or sequence
+copbyo: ldd                     ; loop unrolling x2
         add     a, a
-        jr      z, mailab
-        jr      c, maicoo
-copbyo: ldd
-        add     a, a
-        jr      nc, copbye
+        jr      nc, copbye      ; next bit indicates either literal or sequence
 
-maicoe: ld      bc, 2
-maisie: push    de
-        ld      d, b
-        getbitm
+; determine number of bits used for length (Elias gamma coding)
+maicoe: ld      bc, 2           ; BC=$0002
+maisie: push    de              ; store destination on stack
+        ld      d, b            ; D= 0
+        getbitm                 ; getbit with macro getbitm
       if speed=0
         jr      c, contie
       else
-        jp      c, contie
+        jp      c, contie       ; jp is 2 cycles faster when jump not taken
       endif
-        dec     c
+        dec     c               
 levale: add     a, a
         rl      c
-        rl      b
-        getbitm
-        jr      nc, levale
-        inc     c
-        jr      z, exitdz
-contie: ld      e, (hl)
+        rl      b               ; insert bit on BC
+        getbitm                 ; more bits?
+        jr      nc, levale      ; repeat, final length on BC
+
+; check escape sequence
+        inc     c               ; detect escape sequence
+        jr      z, exitdz       ; end of algorithm
+
+; determine offset
+contie: ld      e, (hl)         ; load offset flag (1 bit) + offset value (7 bits)
         dec     hl
         sll     e
-        jr      nc, offnde
-        add     a, a
-        rl      d
+        jr      nc, offnde      ; if offset flag is set, load 4 extra bits
+        add     a, a            ; load 4 bits by code
+        rl      d               ; odd bits don't need end of byte checking
         getbitm
         rl      d
         add     a, a
@@ -70,13 +75,13 @@ contie: ld      e, (hl)
         getbitm
         ccf
         jr      c, offnde
-        inc     d
-offnde: rr      e
-        ex      (sp), hl
-        ex      de, hl
-        adc     hl, de
+        inc     d               ; equivalent to adding 128 to DE
+offnde: rr      e               ; insert inverted fourth bit into E
+        ex      (sp), hl        ; store source, restore destination
+        ex      de, hl          ; destination from HL to DE
+        adc     hl, de          ; HL = destination + offset + 1
         lddr
-        pop     hl
+        pop     hl              ; restore source address (compressed data)
         add     a, a
     if speed=0
         jr      c, maicoe
@@ -92,20 +97,20 @@ offnde: rr      e
       endif
     endif
 
-exitdz: pop     hl
+exitdz: pop     hl              ; exit path
       if speed=0
-getbit: ld      a, (hl)
+getbit: ld      a, (hl)         ; load another group of 8 bits
         dec     hl
         adc     a, a
       endif
         ret
 
-mailab: ld      a, (hl)
+mailab: ld      a, (hl)         ; save some cycles avoiding call getbit
         dec     hl
         adc     a, a
         jr      nc, copbyo
 
-maicoo: ld      bc, 2
+maicoo: ld      bc, 2           ; repeats code above (maicoe), for odd bits
         push    de
         ld      d, b
         add     a, a
--- dzx7b_medium.asm
+++ dzx7b_medium.asm
@@ -6,44 +6,51 @@
 ;   HL: source address (compressed data)
 ;   DE: destination address (decompressing)
 ; -----------------------------------------------------------------------------
-dzx7:   ld      bc, $8000
-        ld      a, b
-copyby: inc     c
-        ldd
-mainlo: add     a, a
-        call    z, getbit
-        jr      nc, copyby
-        push    de
-        ld      d, c
-        defb    $30
-lenval: add     a, a
+dzx7:   ld      bc, $8000       ; marker bit at MSB of B
+        ld      a, b            ; move marker bit to A
+copyby: inc     c               ; to keep BC value after LDD
+        ldd                     ; copy literal byte
+mainlo: add     a, a            ; read next bit, faster method
+        call    z, getbit       ; call routine only if need to load next byte 
+        jr      nc, copyby      ; next bit indicates either literal or sequence
+
+; determine number of bits used for length (Elias gamma coding)
+        push    de              ; store destination on stack
+        ld      d, c            ; D= 0, assume BC=$0000 here (or $8000)
+        defb    $30             ; avoid next add a, a the first time
+lenval: add     a, a            ; read bit
         call    z, getbit
         rl      c
-        rl      b
-        add     a, a
+        rl      b               ; insert bit on BC
+        add     a, a            ; more bits?
         call    z, getbit
-        jr      nc, lenval
-        inc     c
-        jr      z, exitdz
-        ld      e, (hl)
+        jr      nc, lenval      ; repeat, final length on BC
+
+; check escape sequence
+        inc     c               ; detect escape sequence
+        jr      z, exitdz       ; end of algorithm
+
+; determine offset
+        ld      e, (hl)         ; load offset flag (1 bit) + offset value (7 bits)
         dec     hl
         sll     e
-        jr      nc, offend
-        ld      d, $10
+        jr      nc, offend      ; if offset flag is set, load 4 extra bits
+        ld      d, $10          ; bit marker to load 4 bits
 nexbit: add     a, a
         call    z, getbit
-        rl      d
-        jr      nc, nexbit
-        inc     d
-        srl     d
-offend: rr      e
-        ex      (sp), hl
-        ex      de, hl
-        adc     hl, de
+        rl      d               ; insert next bit into D
+        jr      nc, nexbit      ; repeat 4 times, until bit marker is out
+        inc     d               ; add 128 to DE
+        srl     d               ; retrieve fourth bit from D
+offend: rr      e               ; insert fourth bit into E
+        ex      (sp), hl        ; store source, restore destination
+        ex      de, hl          ; destination from HL to DE
+        adc     hl, de          ; HL = destination + offset + 1
         lddr
-exitdz: pop     hl
-        jr      nc, mainlo
-getbit: ld      a, (hl)
+exitdz: pop     hl              ; restore source address (compressed data)
+        jr      nc, mainlo      ; end of loop. exit with getbit instead ret (-1 byte)
+
+getbit: ld      a, (hl)         ; load another group of 8 bits
         dec     hl
         adc     a, a
         ret
